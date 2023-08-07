3068388571f0a45ccc83411368cf3a88ba894f0f
deecodez <51487896+deecodez@users.noreply.github.com>
2023-08-07 20:37:40 +0100

    inital commit

---
+++ lib/src/features/products/data/fake_products_repository.dart
@@ -0,0 +1,49 @@
+import 'package:ecommerce_app/src/constants/test_products.dart';
+import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FakeProductsRepository {
+  final List<Product> _products = kTestProducts;
+
+  List<Product> getProductsList() {
+    return _products;
+  }
+
+  Product? getProduct(String id) {
+    return _products.firstWhere((product) => product.id == id);
+  }
+
+  Future<List<Product>> fetchProductsList() async {
+    await Future.delayed(const Duration(seconds: 2));
+    return Future.value(_products);
+  }
+
+  Stream<List<Product>> watchProductsList() async* {
+    await Future.delayed(const Duration(seconds: 2));
+    yield _products;
+  }
+
+  Stream<Product?> watchProduct(String id) {
+    return watchProductsList()
+        .map((products) => products.firstWhere((product) => product.id == id));
+  }
+}
+
+final productsRepositoryProvider = Provider<FakeProductsRepository>((ref) {
+  return FakeProductsRepository();
+});
+
+final productListStreamProvider = StreamProvider<List<Product>>((ref) {
+  final productsRepository = ref.watch(productsRepositoryProvider);
+  return productsRepository.watchProductsList();
+});
+
+final productsListFutureProvider = FutureProvider<List<Product>>((ref) {
+  final productsRepository = ref.watch(productsRepositoryProvider);
+  return productsRepository.fetchProductsList();
+});
+
+final productProvider = StreamProvider.family<Product?, String>((ref, id) {
+  final productsRepository = ref.watch(productsRepositoryProvider);
+  return productsRepository.watchProduct(id);
+});
